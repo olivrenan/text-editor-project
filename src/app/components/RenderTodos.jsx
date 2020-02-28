5feb201cd8e7c855ce52fbd9f2bf27412fcbd7e3
Renan Oliveira <ren.moura.oliveira@gmail.com>
2020-02-28 16:11:37 -0300

    implemented deleteTodo action

--- src/app/components/RenderTodos.jsx
+++ src/app/components/RenderTodos.jsx
@@ -1,13 +1,16 @@
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 import { createEditor } from "slate";
 import { Editable, Slate, withReact } from "slate-react";
 import { withHistory } from "slate-history";
 import React, { useCallback, useMemo } from "react";
 
+import { deleteTodo } from "../store/actions";
 import Element from "./Element";
 import Leaf from "./Leaf";
 import Popover from "./Popover";
 
-const RenderTodos = ({ todo }) => {
+const RenderTodos = ({ todo, deleteTodo }) => {
   const editor = useMemo(() => withHistory(withReact(createEditor())), []);
   const renderElement = useCallback(props => <Element {...props} />, []);
   const renderLeaf = useCallback(props => <Leaf {...props} />, []);
@@ -17,7 +20,7 @@ const RenderTodos = ({ todo }) => {
       <Popover
         icon="more_vert"
         listChildren={["Edit", "Delete"]}
-        actionsArray={[]}
+        actionsArray={[() => {}, () => deleteTodo(todo)]}
       />
       <Slate editor={editor} value={todo}>
         <Editable
@@ -31,4 +34,7 @@ const RenderTodos = ({ todo }) => {
   );
 };
 
-export default RenderTodos;
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ deleteTodo }, dispatch);
+
+export default connect(null, mapDispatchToProps)(RenderTodos);
--- src/app/store/actions.js
+++ src/app/store/actions.js
@@ -8,10 +8,19 @@ export const addNewTodo = todo => (dispatch, getState) => {
   const newTodos = [...todos];
   newTodos.push([{ id: uuidv4(), children: [...todo] }]);
 
-  console.log(newTodos);
-
   dispatch({
     type: actionTypes.ADD_TODO,
+    todo: newTodos
+  });
+};
+
+export const deleteTodo = todo => (dispatch, getState) => {
+  const { todos } = getState();
+
+  const newTodos = [...todos].filter(element => todo[0].id !== element[0].id);
+
+  dispatch({
+    type: actionTypes.DELETE_TODO,
     todo: newTodos
   });
 };
